e1a0b11edf056f4cc4881c9f075af291ef309c7c
Ryan Matt <ryan.matt.12@gmail.com>
2025-03-12 19:56:28 -0400

    Attempt an overlap validation on the occupant model
    Abandoned because I don't think it's necessarily in the scope of the
    assessment

--- app/models/occupant.rb
+++ app/models/occupant.rb
@@ -2,4 +2,17 @@ class Occupant < ActiveRecord::Base
   belongs_to :unit
 
   validates :name, presence: true
+  validate :no_overlap_with_other_occupants
+
+  private
+
+  def no_overlap_with_other_occupants
+    if Occupant.where(unit: unit, move_out_date: move_in_date..).any?
+      errors.add(:move_in_date, "can't overlap with another occupant")
+    end
+
+    if Occupant.where(unit: unit, move_in_date: ..move_out_date).any?
+      errors.add(:move_out_date, "can't overlap with another occupant")
+    end
+  end
 end
--- spec/models/occupant_spec.rb
+++ spec/models/occupant_spec.rb
@@ -30,4 +30,34 @@ RSpec.describe Occupant do
   it 'has a move out date' do
     expect(toph.move_out_date).to eq Date.parse("2021-01-01")
   end
+
+  it "does not overlap with other occupants in the same unit" do
+    master_yu = Occupant.new(
+      name: "Master Yu",
+      unit: beifong_estate,
+      move_in_date: Date.parse("2020-12-01")
+    )
+
+    expect(master_yu.valid?).to be false
+
+    the_boulder = Occupant.new(
+      name: "The Boulder",
+      unit: beifong_estate,
+      move_out_date: Date.parse("2020-02-01"),
+    )
+
+    expect(the_boulder.valid?).to be false
+
+    sokka = Occupant.new(
+      name: "Sokka",
+      unit: beifong_estate,
+      move_in_date: Date.parse("2018-01-01"),
+      move_out_date: Date.parse("2018-12-31"),
+    )
+
+    sokka.valid?
+
+    puts sokka.errors.inspect
+    expect(sokka.valid?).to be true
+  end
 end
